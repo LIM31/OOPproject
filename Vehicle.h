d75fce9eba86f441d1c7a86998730fa171eaf06d
Betafite <2301381@sit.singaporetech.edu.sg>
2024-11-01 11:01:30 +0800

    Update Vehicle.h

--- Vehicle.h
+++ Vehicle.h
@@ -2,12 +2,39 @@
 #define VEHICLE_H
 
 #include <string>
+#include <iostream>
+
+// Enum class for EngineType
+enum class EngineType {
+    PETROL,
+    DIESEL,
+    ELECTRIC,
+    HYBRID,
+    UNKNOWN
+};
+
+// Enum class for Color
+enum class Color {
+    RED,
+    BLUE,
+    GREEN,
+    BLACK,
+    WHITE,
+    UNKNOWN
+};
+
+// Enum class for Size
+enum class Size {
+    SMALL,
+    MEDIUM,
+    LARGE,
+};
 
 // Vehicle class
 class Vehicle {
 public:
     // Constructor
-    Vehicle(const std::string& engine = "Unknown", const std::string& color = "Unknown", const std::string& size = "Unknown");
+    Vehicle(EngineType engine = EngineType::UNKNOWN, Color color = Color::UNKNOWN, Size size = Size::UNKNOWN);
 
     // Member function to display vehicle information
     void display() const;
@@ -16,9 +43,14 @@ public:
     void customize();
 
 private:
-    std::string engine;  // Engine type
-    std::string color;   // Color of the vehicle
-    std::string size;    // Size of the vehicle
+    EngineType engine;  // Engine type
+    Color color;        // Color of the vehicle
+    Size size;          // Size of the vehicle
+
+    // Helper functions to convert enums to strings for display
+    static std::string engineTypeToString(EngineType engine);
+    static std::string colorToString(Color color);
+    static std::string sizeToString(Size size);
 };
 
 #endif // VEHICLE_H
