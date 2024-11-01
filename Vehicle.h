73c4658179adfa08bce7cc790e3684c98e8edf85
Betafite <2301381@sit.singaporetech.edu.sg>
2024-11-01 10:35:02 +0800

    Update Vehicle.h

--- Vehicle.h
+++ Vehicle.h
@@ -1,1 +1,19 @@
+#ifndef VEHICLE_H
+#define VEHICLE_H
 
+#include <string>
+
+class Vehicle 
+{
+private:
+   std::string engine;
+   std::string colour;
+   std::string size;
+public:
+
+   
+
+
+
+
+}
