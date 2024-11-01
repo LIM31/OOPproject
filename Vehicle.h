72ec61324bbce75178e713302390a72ca3069301
Betafite <2301381@sit.singaporetech.edu.sg>
2024-11-01 10:52:17 +0800

    Update Vehicle.h

--- Vehicle.h
+++ Vehicle.h
@@ -3,17 +3,22 @@
 
 #include <string>
 
-class Vehicle 
-{
-private:
-   std::string engine;
-   std::string colour;
-   std::string size;
+// Vehicle class
+class Vehicle {
 public:
+    // Constructor
+    Vehicle(const std::string& engine = "Unknown", const std::string& color = "Unknown", const std::string& size = "Unknown");
 
-   
+    // Member function to display vehicle information
+    void display() const;
 
+    // Member function to customize vehicle
+    void customize();
 
+private:
+    std::string engine;  // Engine type
+    std::string color;   // Color of the vehicle
+    std::string size;    // Size of the vehicle
+};
 
-
-}
+#endif // VEHICLE_H
